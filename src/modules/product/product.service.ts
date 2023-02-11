856624dd33977efabfe41af31ba2fc5d9f695c9a
gabrielsimplicio00 <gaasimplicio@gmail.com>
2023-02-10 23:35:31 -0300

    DTO adicionado para a rota de produto

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from "@nestjs/common";
+import { ReadProductsDto } from "./dto/read-products.dto";
 
 @Injectable()
 export class ProductService {
 
-  async getProducts(): Promise<object> {
+  async getProducts(): Promise<object | ReadProductsDto[]> {
     const fetchProducts = await fetch('https://mockend.com/juunegreiros/BE-test-api/products')
     const products = await fetchProducts.json()
     return products
