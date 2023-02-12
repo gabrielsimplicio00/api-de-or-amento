11bdef285564a88087ceec2412d3f71db9a7c6d0
gabrielsimplicio00 <gaasimplicio@gmail.com>
2023-02-12 14:32:30 -0300

    refatoração e comentários de código

---
+++ src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class AppService {
+  routes(): object {
+    return {
+      listarUsuarios: "localhost:3000/users",
+      listarProdutos: "localhost:3000/products",
+      listarUsuarioEProdutosPeloId: "localhost:3000/users/:id?products=1,2,3",
+    }
+  }
+}
--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -3,10 +3,13 @@ import { ReadProductsDto } from "./dto/read-products.dto";
 
 @Injectable()
 export class ProductService {
-
+  // busca todos os produtos no mockend e retorna um array nos moldes de ReadProductsDto, pra deixar o modelo do objeto padronizado
   async getProducts(): Promise<object | ReadProductsDto[]> {
-    const fetchProducts = await fetch('https://mockend.com/juunegreiros/BE-test-api/products')
-    const products = await fetchProducts.json()
-    return products
+    try {
+      const products = await (await fetch('https://mockend.com/juunegreiros/BE-test-api/products')).json()
+      return products
+    } catch (error) {
+      throw new Error(error)
+    }
   }
 }
